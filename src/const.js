f8c47c093dba0dba10a47ab96bd75b274f410c40
Milton Jones <miltonejones@gmail.com>
2024-02-03 17:03:41 -0600

    updated domains

--- src/const.js
+++ src/const.js
@@ -2,13 +2,8 @@ export const ERR_IMAGE =
   "https://s3.amazonaws.com/sploosh.me.uk/assets/no-img-women.jpg";
 export const DEFAULT_IMAGE =
   "https://s3.amazonaws.com/sploosh.me.uk/assets/XXX.jpg";
-export const includedDomains = [
-  "javdoe.sh",
-  "eporner.com",
-  "xvideos.com",
-  "dmm.co.jp",
-];
-export const excludedDomains = ["javdoe.com", "javfinder.la"];
+export const includedDomains = ["javdoe.sh", "xvideos.com", "dmm.co.jp"];
+export const excludedDomains = ["javdoe.com", "eporner.com", "javfinder.la"];
 
 export const approvedDomain = "javdoe.sh";
 
